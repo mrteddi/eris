1115ceb4f3e0eed4f109e2d8e16afbfc2ceb8fef
mrteddi <crmoening@comcast.net>
2020-04-27 16:11:14 -0700

    Packet filtering

--- daemon/main.go
+++ daemon/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"time"
 
 	"github.com/google/gopacket"
@@ -10,19 +11,39 @@ import (
 )
 
 func packets() {
+
+	// Building the filter string to check for wanted ports
+	ports := []int{21, 22}
+	filterStr := "tcp and ("
+	for i, p := range ports {
+		p := strconv.Itoa(p)
+		if i != len(ports)-1 {
+			filterStr += "port " + string(p) + " or "
+		} else {
+			filterStr += "port " + string(p)
+		}
+
+	}
+	filterStr += ")"
+
 	if handle, err := pcap.OpenLive("wlp1s0", 1600, true, pcap.BlockForever); err != nil {
+		// Start pcap capture on the wireless interface
 		panic(err)
-	} else if err := handle.SetBPFFilter("tcp and port 22"); err != nil { // optional
+	} else if err := handle.SetBPFFilter(filterStr); err != nil {
+		// Sets filter to capture on, panic if any error
 		panic(err)
 	} else {
 		packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 		for packet := range packetSource.Packets() {
+			// Get the TCP layer from the captured packet
 			tcpInfo := packet.Layer(layers.LayerTypeTCP).(*layers.TCP)
 
-			if tcpInfo.SrcPort == 22 {
-				fmt.Println("Packet goroutine| SSH FOUND!")
+			// Check for matching ports
+			for _, p := range ports {
+				if tcpInfo.SrcPort == layers.TCPPort(p) {
+					fmt.Printf("Packet goroutine| Port %d found!\n", p)
+				}
 			}
-			//fmt.Println(tcpInfo.SrcPort)
 		}
 	}
 }
